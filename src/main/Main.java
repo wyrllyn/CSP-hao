e0aefe22aa2487cf8bc59593361f7a907ea7dfb2
X3N0-Life-Form <adrarsis@laposte.net>
2014-04-14 18:27:03 +0200

    deal with args you idiot

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import core.Resolution;
 import core.Solution;
+import core.Solution.Init;
 
 public class Main {
 
@@ -28,8 +29,19 @@ public class Main {
 	}
 
 	private static void dealWithArgs(String[] args) {
-		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i]) {
+			case "-knight":
+				init = Init.KNIGHT;
+				break;
+			case "-diag":
+				init = Init.DIAG;
+				break;
+			case "-size":
+				size = Integer.parseInt(args[++i]);
+				break;
+			}
+		}
 	}
 
 }
